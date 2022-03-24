ad29810e6076a7155806da641682b5911843d114
Misty Duhart <mystie.smith@gmail.com>
2022-03-23 23:54:43 -0400

    html and css and javascript files

--- javascript.js
+++ javascript.js
@@ -28,22 +28,16 @@ var questions = [
 
 ];
 
-// Declared variables
 var score = 0;
 var questionIndex = 0;
 
-// Start working code 
-// Declared variables
 var currentTime = document.querySelector("#currentTime");
 var timer = document.querySelector("#startTime");
 var questionsDiv = document.querySelector("#questionsDiv");
 
 
-// Seconds left is 15 seconds per question:
 var secondsLeft = 60;
-// Holds interval time
 var holdInterval = 0;
-// Holds penalty time
 var penalty = 10;
 // Creates new element
 var ulCreate = document.createElement("ul");
